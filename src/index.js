5a7ed16950bf0f160e99d86d15997ebb54cff03d
Michael Bradley <mbradleyis@gmail.com>
2016-09-04 12:45:58 -0700

    first build

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <div>hey</div>,
   document.getElementById('root')
 );
